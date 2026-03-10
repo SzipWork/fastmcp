10bb47be4c5f86bc483edeedeead32c8e3de9419
SzipWork <schoudhury7.work@gmail.com>
2026-03-10 15:23:41 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastmcp import FastMCP
+import random
+import json
+mcp = FastMCP("Simple Calculator Server")
+
+
+
+#Tool
+@mcp.tool
+def add(a:int, b:int) -> int:
+    """Add two numbers together.
+    
+    Args:
+    a: First Number
+    b: Second number
+    
+    Returns:
+    The sum of a and b
+    """
+    return a+b
+
+@mcp.tool
+def random_number(min_val: int=1, max_val: int=100) -> int:
+    """Generate a random number within a range.
+    
+    Args:
+    min_val: Minimum value (default:1)
+    max_val: Maximum value (default:100)
+    
+    Returns:
+    A rnadom integer between min_val and max_val
+    """
+
+    return random.randint(min_val, max_val)
+
+#Resource: Server Information
+@mcp.resource("info://server")
+def server_info() -> str:
+    """Get information anout this server"""
+    info = {
+        "name": "Simple Ca;lculator Server",
+        "version": "1.0.0",
+        "description": "A basic MCP Server with math tools",
+        "tools": ["add", "random_number"]
+    }
+    return json.dumps(info, indent=2)
+
+#Initialize Server
+
+if __name__=="__main__":
+    mcp.run(transport="http", host="0.0.0.0", port=8001)
